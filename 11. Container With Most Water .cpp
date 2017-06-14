dbe016ba9230b44062def5eebb27bd7a77460710
mehmet <mehmetozdemir94@gmail.com>
2017-06-14 18:46:20 +0300

    11. Container With Most...

---
+++ 11. Container With Most Water .cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+  		int left = 0, right = height.size() - 1, maxArea = 0;  		
+  		while(left != right){
+  			maxArea = max(maxArea, (right - left) * min(height[left], height[right]));
+  			if(height[right] > height[left])
+  				left++;
+  			else
+  				right++;
+  		}	      
+    }
+    return maxArea;
+};
+
+int main(){
+
+}
