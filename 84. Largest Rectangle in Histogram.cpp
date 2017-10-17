ab91fc01d286a90a85b57cc30bab21bd82c02267
Mehmet Ozdemir <ozdemir08@users.noreply.github.com>
2017-10-17 16:04:11 +0300

    84. Largest Rectangle in Histogram with new solution

--- 84. Largest Rectangle in Histogram.cpp
+++ 84. Largest Rectangle in Histogram.cpp
@@ -2,66 +2,100 @@
 #include <vector>
 #include <set>
 #include <algorithm>
+#include <stack>
 
 using namespace std;
 
-// first -> height, second -> index
-typedef pair<int,int> Height;
+/**
+    Solution 2 O(N) time complexity
+**/
 
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-
-        int n = heights.size();
-        set<int> limits;
-        std::set<int>::iterator itlow,itup;
-
-        vector<Height> sorted;
-        for(int i=0;i<n;i++)
-            sorted.push_back(make_pair(heights[i], i));
-
-        // sort according to height
-        sort(sorted.begin(), sorted.end());
-
+        stack<int> indices;
         int res = 0;
-        vector<int> toAdd;
-        limits.insert(-1);
-        limits.insert(n);
-
-        for(int i=0;i<n;i++){
-            int height = sorted[i].first;
-            int index = sorted[i].second;
-
-            // handling same heights
-            if(!toAdd.empty() && heights[toAdd[0]] != height){
-                for(int j=0;j<toAdd.size();j++)
-                    limits.insert(toAdd[j]);
-
-                toAdd.clear();
+        int n = heights.size();
+        for(int i=0;i<=n;i++){
+            int h = (i == n ? 0 : heights[i]);
+            if(indices.empty() || heights[indices.top()] <= h){
+                indices.push(i);
             }
-            toAdd.push_back(index);
+            else{
+                int last = indices.top();
+                indices.pop();
 
-            itlow = limits.lower_bound(index);
-            itup = limits.upper_bound(index);
-
-            int left = *(--itlow), right = *itup;
-            int area = (right - left - 1) * height;
-            res = max(res, area);
+                int interval = 0;
+                if(indices.empty())
+                    interval = i;
+                else
+                    interval = i - indices.top() - 1;
+                
+                int area = interval * heights[last];
+                res = max(res, area);
+                i--;
+            }
         }
-        
         return res;
     }
 };
 
+/**
+    Solution 1 O(N log(N)) time complexity
+**/
+// first -> height, second -> index
+// typedef pair<int,int> Height;
+
+// class Solution {
+// public:
+//     int largestRectangleArea(vector<int>& heights) {
+
+//         int n = heights.size();
+//         set<int> limits;
+//         std::set<int>::iterator itlow,itup;
+
+//         vector<Height> sorted;
+//         for(int i=0;i<n;i++)
+//             sorted.push_back(make_pair(heights[i], i));
+
+//         // sort according to height
+//         sort(sorted.begin(), sorted.end());
+
+//         int res = 0;
+//         vector<int> toAdd;
+//         limits.insert(-1);
+//         limits.insert(n);
+
+//         for(int i=0;i<n;i++){
+//             int height = sorted[i].first;
+//             int index = sorted[i].second;
+
+//             // handling same heights
+//             if(!toAdd.empty() && heights[toAdd[0]] != height){
+//                 for(int j=0;j<toAdd.size();j++)
+//                     limits.insert(toAdd[j]);
+
+//                 toAdd.clear();
+//             }
+//             toAdd.push_back(index);
+
+//             itlow = limits.lower_bound(index);
+//             itup = limits.upper_bound(index);
+
+//             int left = *(--itlow), right = *itup;
+//             int area = (right - left - 1) * height;
+//             res = max(res, area);
+//         }
+        
+//         return res;
+//     }
+// };
+
 int main(){
     std::vector<int> v;
     v.push_back(2);
-    v.push_back(5);
-    v.push_back(5);
-    v.push_back(3);
-    v.push_back(1);
-    // v.push_back(2);
-    // v.push_back(1);
+    v.push_back(0);
+    v.push_back(2);
     Solution s;
     cout << s.largestRectangleArea(v) << endl;
 
