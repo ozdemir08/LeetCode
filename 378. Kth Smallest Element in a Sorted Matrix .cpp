d8cc314a2f7f7afb566d9a734247536e7851b5f1
Mehmet Ozdemir <ozdemir08@users.noreply.github.com>
2017-10-26 11:01:32 +0300

    378. Kth Smallest Element in a Sorted Matrix .cpp

--- 378. Kth Smallest Element in a Sorted Matrix .cpp
+++ 378. Kth Smallest Element in a Sorted Matrix .cpp
@@ -4,49 +4,36 @@
 
 using namespace std;
 
-class Number{
-public:
-	int num, row;
-	Number(int n, int r){
-		num = n;
-		row = r;
-	}
-};
-
-bool cmp(Number a, Number b){
-	return a.num > b.num;
-}
-
 class Solution {
+private:
+	int countSmaller(vector<vector<int> >& matrix, int target){
+		int count = 0;
+		int n = matrix.size();
+		int i = n-1;
+		for(int j=0;j<n && i>=0;j++){
+			if(matrix[i][j] > target){
+				i--; 
+				j--;
+			}
+			else
+				count += i + 1;
+		}
+		return count;
+	}
 public:
     int kthSmallest(vector<vector<int> >& matrix, int k) {
-    	int n = matrix.size();
-        vector<int> indices(n, 0);
-        
-        vector<Number> minHeap;
-        make_heap(minHeap.begin(), minHeap.end(), cmp);
-        for(int row=0;row<n;row++){
-        	int col = indices[row]++;
-        	int num = matrix[row][col];
-        	minHeap.push_back(Number(num, row));
-        	push_heap(minHeap.begin(), minHeap.end(), cmp);
-	    }
-
-	    Number lastMin(-1, -1);
-	    for(int i=0;i<k;i++){
-	    	lastMin = minHeap[0];
-	    	pop_heap(minHeap.begin(), minHeap.end(), cmp);
-	    	minHeap.pop_back();
-
-	    	int row = lastMin.row;
-	    	if(indices[row] < n){
-	    		int col = indices[row] ++;
-	    		int num = matrix[row][col];
-	    		minHeap.push_back(Number(num, row));
-	        	push_heap(minHeap.begin(), minHeap.end(), cmp);
-			}
-	    }
-	    return lastMin.num;
+    	int N = matrix.size();
+    	int left = matrix[0][0], right = matrix[N-1][N-1];
+    	while(left <= right){
+    		cout << left << ' ' << right << endl;
+    		int mid = (left + right) / 2;
+    		int count = countSmaller(matrix, mid); 
+    		if(count < k)
+    			left = mid + 1;
+    		else
+    			right = mid - 1;
+    	}
+    	return left;
     }
 };
 
@@ -56,5 +43,5 @@ int main(){
 	for(int i=0;i<5;i++)
 		for(int j=0;j<5;j++)
 			matrix[i][j] = i + j;
-	cout << s.kthSmallest(matrix, 7) << endl;
+	cout << s.kthSmallest(matrix, 14) << endl;
 }
