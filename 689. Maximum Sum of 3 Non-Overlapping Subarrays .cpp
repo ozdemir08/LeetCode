7d77a38e4c0aefe83121b2185dff281f56fa3930
Mehmet Ozdemir <ozdemir08@users.noreply.github.com>
2017-10-27 23:29:33 +0300

    689. Maximum Sum of 3 Non-Overlapping Subarrays .cpp

---
+++ 689. Maximum Sum of 3 Non-Overlapping Subarrays .cpp
@@ -0,0 +1,60 @@
+class Solution {
+private:
+    vector< vector<int> > DP;
+    vector< vector<bool> > flag;
+    vector<int> sum;
+    int N, K;
+    
+    int findSubArraySum(int i){
+        if(!i)
+            return sum[K-1];
+        return sum[i+K-1] - sum[i-1];
+    }
+    
+    void init(vector<int>&nums, int k){
+        N = nums.size();
+        K = k;
+        // +1 s for edge cases.
+        DP = vector< vector<int> >(4, vector<int>(N+1, 0));
+        flag = vector< vector<bool> >(4, vector<bool>(N+1, false));
+        sum = vector<int>(N, 0);
+        sum[0] = nums[0];
+        for(int i=1;i<N;i++)
+            sum[i] = sum[i-1] + nums[i];
+    }
+
+public:
+    vector<int> maxSumOfThreeSubarrays(vector<int>& nums, int k) {
+        init(nums, k);
+        
+        // fill DP
+        for(int i=1;i<=3;i++)
+            for(int j=N-K;j>=0;j--){
+                // Due to N+1 in DP's definition, no need to handle edge cases.
+                int right = DP[i][j+1];
+                int bottom = findSubArraySum(j) + DP[i-1][j+K];
+                if(bottom >= right){
+                    DP[i][j] = bottom;
+                    // flag shows ith subarray starts with jth column
+                    flag[i][j] = true;
+                }
+                else
+                    DP[i][j] = right;
+            }
+        
+        // find begin index of three row.
+        vector<int> res;
+        int i=3, j=0;
+        while(i>0 && j<=N-1){
+            if(flag[i][j]){
+                res.push_back(j);
+                i--;
+                j+=K;
+            }
+            else
+                j++;
+        }
+
+        return res;
+    }
+};
